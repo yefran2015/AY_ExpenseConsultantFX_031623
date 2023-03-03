9d37b3cea5c5996eb2f1af47e53fe472894d757a
yefran2015 <yefran2015@gmail.com>
2023-03-02 20:17:40 -0600

    button add added

--- main/gui_v1/transaction_records/GUI_NewAccountP.java
+++ main/gui_v1/transaction_records/GUI_NewAccountP.java
@@ -4,11 +4,14 @@ import gui_v1.automation.GUI_ElementCreator;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI_NewAccountP extends JPanel{
+public class GUI_NewAccountP extends JPanel implements ActionListener {
     private JComboBox  jcmbBank = GUI_ElementCreator.newJComboBox(new String[]{"Bank","Bank2","Bank3"});
     private JTextField jtfAcctNum= GUI_ElementCreator.newTextField("Account Number");
     private JTextField jtfAcctNick= GUI_ElementCreator.newTextField("Account Nick");
+    private JButton jbtnAdd = GUI_ElementCreator.newJButton("Add This Account");
 
     public GUI_NewAccountP(){
         setLayout(new BorderLayout());
@@ -21,6 +24,30 @@ public class GUI_NewAccountP extends JPanel{
         pBox.add(GUI_ElementCreator.newTextLabel("Bank:"));
         pBox.add(jcmbBank);
         add(pBox, BorderLayout.CENTER);
+        jbtnAdd.addActionListener(this);
+        add(jbtnAdd, BorderLayout.SOUTH);
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()== jbtnAdd){
+            String msg = "Do you really want to save this account:";
+            msg+="\n";
+            msg+="Account #: "+ jtfAcctNum.getText().trim();
+            msg+="\n";
+            msg+="Account Nick:"+ jtfAcctNick.getText().trim();
+            msg+="\n";
+            msg+="Bank of Account:"+ jcmbBank.getSelectedItem().toString().trim();
+            msg+="\n";
+            msg+="After Clicking Yes button this account will be added to your accounts";
+            int answr = JOptionPane.showOptionDialog(null, msg, "Adding and Storing Account!",
+                    JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE, null, null, JOptionPane.NO_OPTION);
+            if(answr == JOptionPane.YES_OPTION){
+                System.exit(JFrame.EXIT_ON_CLOSE);
+            }else{
+
+            }
+        }
     }
 }
