a91a97bbee19ffacf6b57e0170dc0d8f2b85b77d
yefran2015 <yefran2015@gmail.com>
2023-03-03 03:49:36 -0600

    New Account Nick adding to combo box added
    line of code added to --> call method of Manual Transaction Window for adding new Account Nickname into GUI Drop down list

--- main/gui_v1/action_processors/NewAccountProgrammableHandler.java
+++ main/gui_v1/action_processors/NewAccountProgrammableHandler.java
@@ -2,29 +2,22 @@ package gui_v1.action_processors;
 
 import javax.swing.*;
 
+import static gui_v1.transaction_records.GUI_ManualTransactionsEntryP.addAccountNickToComboBox;
+
 public class NewAccountProgrammableHandler {
     private String strAcctNum;
     private String strAccntNick;
     private String strBank;
-//    private String descr;
-//    private  String memo;
-//    private String amount;
-//    private String customCategory;
-//    public NewAccountProgrammableHandler(String _acct, String _date, String _refN, String _descr, String _memo,
-//                                          String _amount, String _customCategory) {
     public NewAccountProgrammableHandler(String _strAcctNum, String _strAccntNick, String _strBank){
         strAcctNum = _strAcctNum;
         strAccntNick = _strAccntNick;
         strBank = _strBank;
-//        descr = _descr;
-//        memo = _memo;
-//        amount = _amount;
-//        customCategory = _customCategory;
-
+        addAccountNickToComboBox(_strAccntNick);
         showNewManualEntryInfo();
     }
     private void showNewManualEntryInfo(){
-        String regInfo = "User New Accounts :\nAccount #:"+ strAcctNum+ "\nAccount NickName: " + strAccntNick +"\nBank "+ strBank;
+        String regInfo = "User New Accounts :\nAccount #:"+ strAcctNum+ "\nAccount NickName: " + strAccntNick
+                +"\nBank "+ strBank;
 
 
         JOptionPane.showMessageDialog(null, regInfo,  "New Account Data", JOptionPane.INFORMATION_MESSAGE);
