7d762319b8a2e358a0748c68bfc23d9e9d7d07ef
yefran2015 <yefran2015@gmail.com>
2023-03-03 07:38:53 -0600

    method for date sorting is updated
    Previous date sorting algorithm was not sort in both Descending and Ascending orders only in one with result for another as not correct.
    This fixed sorting is working well to sort  transaction rows by date column.

--- main/gui_v1/RecordsTable_CustomMethods.java
+++ main/gui_v1/RecordsTable_CustomMethods.java
@@ -3,13 +3,14 @@ package gui_v1;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 import java.util.Comparator;
+import java.util.Date;
 
 /**
  * @author Andrey Y
  * @author  Pavel
  * @author Sam
- * @version 0.0.1
- * @serial 030220231659
+ * @version 0.0.2
+ * @serial 030320230735
  *  This class is place for accumulating all custom methods needed
  *  for SPAM Teams Expense Consultant App to Run, and showing
  *  correct results.
@@ -17,9 +18,9 @@ import java.util.Comparator;
  */
 public class RecordsTable_CustomMethods {
     /**
-     * @param m   table model of table to set custom row sorting
-     * @return -- custom row sorting for specified column of tables
+     * @param m  -- table model of table to set custom row sorting
      * @column -- column to which apply this sorting
+     * @return -- custom row sorting for specified column of tables
      *  To set custom sorting of rows by some column tablee need this satment
      *   table.setRowSorter(getCustomRowSorter(table.getModel()), culumnNum);
      *
@@ -40,41 +41,35 @@ public class RecordsTable_CustomMethods {
         });
         return sorter;
     }
+    /**
+     * @param m -- table model of table to set custom row sorting
+     * @column -- column to which apply this sorting
+     * @return -- custom row sorting for specified column of tables
+     *
+     * This custom method is for performing Transaction Table Rows Sorting by
+     * date column.
+     *
+     *
+     */
     public static TableRowSorter getCustomRowSorterByDate(TableModel m, int column){
         var sorter = new TableRowSorter<TableModel>(m);
         sorter.setComparator(column, new Comparator<String>() {
             @Override
             public int compare(String c1, String c2) {
-                Double n1 = null;
-                Double n2 = null;
+                Date d1;
+                Date d2;
                 String[] date1arr = c1.split("/");
                 String[] date2arr = c2.split("/");
-                Integer y1 = Integer.parseInt(date1arr[0]);
-                Integer m1 = Integer.parseInt(date1arr[1]);
-                Integer d1 = Integer.parseInt(date1arr[2]);
-                Integer y2 = Integer.parseInt(date2arr[0]);
-                Integer m2 = Integer.parseInt(date2arr[1]);
-                Integer d2 = Integer.parseInt(date2arr[2]);
-                if(y1<y2){
+                d1 = new Date(Integer.parseInt(date1arr[0]),Integer.parseInt(date1arr[1]),Integer.parseInt(date1arr[2]));
+                d2 = new Date(Integer.parseInt(date2arr[0]),Integer.parseInt(date2arr[1]),Integer.parseInt(date2arr[2]));
+                int answr = d1.compareTo(d2);
+                if(answr==0){
+                    return 0;
+                }else if (answr<0){
                     return -1;
-                }else if(y1>y2){
-                    return 1;
-                }else if(y1==y2){
-                    if(m1<m2){
-                        return -1;
-                    }else if(m1>m2){
-                        return 1;
-                    }else{
-                        if(d1<d2){
-                            return -1;
-                        } else if(d1>d2){
-                            return 1;
-                        }else{
-                            return 0;
-                        }
-                    }
+                }else {
+                   return 1;
                 }
-                return 0;
             }
         });
         return sorter;
@@ -107,7 +102,6 @@ public class RecordsTable_CustomMethods {
                     n2*= -1;
                 }
 //                o(n1 + " "+ n2);
-
                 if (n1 <= n2) {
                     return -1;
                 } else if (n1 > n2) {
@@ -118,7 +112,40 @@ public class RecordsTable_CustomMethods {
         });
         return sorter;
     }
-
+    public static TableRowSorter getCustomRowSorterByDate2(TableModel m, int column){
+        var sorter = new TableRowSorter<TableModel>(m);
+        sorter.setComparator(column, new Comparator<String>() {
+            @Override
+            public int compare(String c1, String c2) {
+                Double n1 = null;
+                Double n2 = null;
+                String[] date1arr = c1.split("/");
+                String[] date2arr = c2.split("/");
+                Integer y1 = Integer.parseInt(date1arr[0]);
+                Integer m1 = Integer.parseInt(date1arr[1]);
+                Integer d1 = Integer.parseInt(date1arr[2]);
+                Integer y2 = Integer.parseInt(date2arr[0]);
+                Integer m2 = Integer.parseInt(date2arr[1]);
+                Integer d2 = Integer.parseInt(date2arr[2]);
+                if((y1 == y2) && (m1 == m2) && (d1 == d2)){
+                    return 0;
+                }else if((y1 == y2) && (m1 == m2) && (d1 < d2)) {
+                    return -1;
+                }else if((y1 == y2) && (m1 == m2) && (d1 > d2)){
+                    return 1;
+                }else if((y1 == y2) && (m1 < m2)){
+                    return -1;
+                }else if((y1 == y2) && (m1 > m2)){
+                    return 1;
+                }else if(y1 < y2){
+                    return -1;
+                }else{
+                    return 1;
+                }
+            }
+        });
+        return sorter;
+    }
 //    private static void o(Object o){
 //        System.out.println(o+"");
 //    }
