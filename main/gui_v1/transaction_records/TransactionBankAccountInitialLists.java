9c5ca67b7b1be0a2484249e874e8e32af9c4b025
yefran2015 <yefran2015@gmail.com>
2023-03-07 14:15:37 -0600

    Class for initial selection lists
    This --> TransactionBankAccountInitialLists class --> for all initial lists of all comboboxes of GUI Windows , they are loading at the time of Windows Creation.
    Also --> TransactionBankAccountInitialLists class --> handlers selection Items (for creating/adding NEW BANK or ACCNT NICKs  by selecting item "NEW" or "NEW BANK")  and make them always last item of dropDown comboboxes lists. Also "OTHER" in category selection List always keeping as last item in shown list.
    More Items to any of lists ---> can be added as Array of Items or single item. 
    ALL unsaved--> temporal user added selection category will be added to this class, and if not stored discarded to default(already stored) lists.

---
+++ main/gui_v1/transaction_records/TransactionBankAccountInitialLists.java
@@ -0,0 +1,97 @@
+package gui_v1.transaction_records;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ *  This class to handle all Categories Lists of GUI RUN TIme.
+ */
+public class TransactionBankAccountInitialLists {
+
+
+    private static LinkedList<String> bankList = new LinkedList<>(Arrays.asList("NEW BANK"));
+    private static LinkedList<String> acctNicksList = new LinkedList<>(Arrays.asList("NEW"));
+    private static LinkedList<String> transactionCategoriesList = new LinkedList<>(Arrays.asList("OTHER"));
+
+    private static TransactionBankAccountInitialLists instance = null;
+    private TransactionBankAccountInitialLists(){
+        instance = this;
+        loadTestingData();
+
+    }
+    public static TransactionBankAccountInitialLists getInstance(){
+        if(instance == null){
+            instance = new TransactionBankAccountInitialLists();
+        }
+        return instance;
+    }
+
+    private void loadTestingData(){
+        String[] bankTestingArr = new String[]{"Wells Fargo", "US Bank", "Bank Of America"};
+        addBanksToList(bankTestingArr);
+        String[] accntNicksTestingArr = new String[]{ "Work Accnt","Family Use Accnt","Secret Saving Accnt"};
+        addAccntNicksToList(accntNicksTestingArr);
+        String[] trnsCategoriesTestingArr = new String[]{ "Food","Car Repair","Mortgage", "Car insurance", "Fun"};
+        addTransactionCategoriessToList(trnsCategoriesTestingArr);
+
+    }
+
+    protected void addTransactionCategoriessToList(String[] transactionCategoriesArr) {
+        String last = transactionCategoriesList.removeLast();
+        transactionCategoriesList.addAll(Arrays.stream(transactionCategoriesArr).toList());
+        transactionCategoriesList.addLast(last);
+    }
+    protected void addTransactionCategoryToList(String transactionCategory) {
+        transactionCategoriesList.add(bankList.size()-1, transactionCategory);
+    }
+    protected static void addBanksToList(String[] banks){
+        String last = bankList.removeLast();
+        bankList.addAll(Arrays.stream(banks).toList());
+        bankList.addLast(last);
+    }
+    protected static void addBankToList(String bank){
+        bankList.add(bankList.size()-1, bank);
+    }
+    protected static void addAccntNicksToList(String[] accountNicks){
+        String last = acctNicksList.removeLast();
+        acctNicksList.addAll(Arrays.stream(accountNicks).toList());
+        acctNicksList.addLast(last);
+    }
+    protected static void addAccntNickToList(String accountNick){
+        acctNicksList.add(acctNicksList.size()-1, accountNick);
+    }
+
+    protected  String[]  getTransCategoryist(){
+
+        return getAsStringArr(transactionCategoriesList);
+//        String[] out = new String[transactionCategoriesList.size()];
+//        for(int i=0; i< transactionCategoriesList.size(); i++){
+//            out[i] = new String(transactionCategoriesList.get(i));
+//        }
+//        return out;
+    }
+    protected  String[]  getBanksList(){
+        return getAsStringArr(bankList);
+//        String[] out = new String[bankList.size()];
+//        for(int i=0; i< bankList.size(); i++){
+//            out[i] = new String(bankList.get(i));
+//        }
+//        return out;
+    }
+    protected  String[]  getAccntNicksList(){
+//        String[] out = new String[acctNicksList.size()];
+//        for(int i=0; i< acctNicksList.size(); i++){
+//            out[i] = new String(acctNicksList.get(i));
+//        }
+//        return out;
+        return getAsStringArr(acctNicksList);
+    }
+
+    private String[] getAsStringArr(LinkedList<String> ll){
+        String[] out = new String[ll.size()];
+        for(int i=0; i< ll.size(); i++){
+            out[i] = new String(ll.get(i));
+        }
+        return out;
+    }
+}
